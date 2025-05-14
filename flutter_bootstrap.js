833d41fc5eae9e0ee63b2e409a089bd37e78a1e3
emilianocl <emilianocl@users.noreply.github.com>
2025-05-14 02:34:13 +0000

    deploy: f61b3130acc8a53a6318690b7854172fb0fcfd97

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2741546126"
+    serviceWorkerVersion: "578008351"
   }
 });
