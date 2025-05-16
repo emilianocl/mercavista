92ce7fdec161be981443c6ad0bad2b1478783ab4
emilianocl <emilianocl@users.noreply.github.com>
2025-05-16 00:52:55 +0000

    deploy: d62dfee3ce96f1a26bf8528f7f4f8616b44ec090

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,13 +28,13 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "index.html": "f35a36364460ce0400ccee603effa376",
 "/": "f35a36364460ce0400ccee603effa376",
-"flutter_bootstrap.js": "622992e971fb7bcbb9f24135b2e3e0aa",
+"flutter_bootstrap.js": "182fe7c5bd3af4504d18c14c30385c57",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "474223b92a3dceee2e3902c4a201c63b"};
+"main.dart.js": "8393d6da8dcd4ce81627f910cbe45d66"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
